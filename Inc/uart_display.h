db0a16b87fcf6f9bea6345ac9ef358ebf2b46d52
zhengkun2013 <zhengkun2013@hotmail.com>
2020-11-22 14:20:06 -0500

    Started working on the instructions and edited some display contents

--- Inc/uart_display.h
+++ Inc/uart_display.h
@@ -9,43 +9,46 @@
 // User input messages
 char user_response[1] = "";
 
+// Instruction
+char print_instruction[] = "";
+
 // Prompt messages
 char count_down[] = "54321";
-char start_menu_message[] = "\n================= Press \"s\" to start the game! ==================";
-char game_starting_message[] = "\n============== The game will now be starting... Get ready in ==============\n";
+char start_menu_message[] = "\r\n================= Press \"s\" to start the game! ==================";
+char game_starting_message[] = "\r\n============== The game will now be starting... Get ready in ==============\r\n";
 char crash_message[] = "============= GAME OVER - You have crashed ===============";
 
 // Flight data
-char fd_title[] = "\n================== FLIGHT DATA =================";
+char fd_title[] = "\r\n================== FLIGHT DATA =================";
 char fd_first_row[40] = "";
-char fd_second_row[65] = "\nHorizontal Position Left: 0 m \tHorizontal Position Right: 0 m";
+char fd_second_row[65] = "\r\nHorizontal Position Left: 0 m \tHorizontal Position Right: 0 m";
 char fd_third_row[70] = "";
 uint32_t magnitude;
 
 // ASCII Images of plane
-static char plane_straight[] = "\n\t\t     \\ /\n\t\t+----o0o----+";
-static char plane_right[] = "\n\t\t+--__   / /\n\t\t      -o0o__\n\t\t            --+";
-static char plane_left[] = "\n\t\t      \\ \\   __+\n\t\t     __o0o--\n\t\t+__--";
-static char plane_up[] = "\n\t\t      /\\\n\t\t   _/ |0| \\_\n\t\t  /   |  |  \\\n\t\t /  _ \\ / _  \\\n\t\t+__/ \\o0o/ \\__+";
-static char plane_down[] = "\n\t\t     \\ /\n\t\t+____o0o____+\n\t\t \\   | |   /\n\t\t  \\__| |__/\n\t\t     \\/";
+static char plane_straight[] = "\r\n\t\t     \\ /\r\n\t\t+----o0o----+";
+static char plane_right[] = "\r\n\t\t+--__   / /\r\n\t\t      -o0o__\r\n\t\t            --+";
+static char plane_left[] = "\r\n\t\t      \\ \\   __+\r\n\t\t     __o0o--\r\n\t\t+__--";
+static char plane_up[] = "\r\n\t\t      / \\\r\n\t\t   _/ |0| \\_\r\n\t\t  /  |   |  \\\r\n\t\t /  _ \\ / _  \\\r\n\t\t+__/ \\o0o/ \\__+";
+static char plane_down[] = "\r\n\t\t     \\ /\r\n\t\t+____o0o____+\r\n\t\t \\   | |   /\r\n\t\t  \\__| |__/\r\n\t\t     \\ /";
 
 void flightDataFirstRow(char *first_row, int32_t altitude, uint16_t speed) {
-	snprintf(first_row,40,"\nAltitude: %06u m \tSpeed: %d km/h", altitude, speed);
+	snprintf(first_row,40,"\r\nAltitude: %06u m \tSpeed: %d km/h", altitude, speed);
 }
 
 void flightDataSecondtRow(char *second_row, int32_t horizontal) {
 	magnitude = abs(horizontal);
 	if (horizontal < 0) {
-		snprintf(second_row,65,"\nHorizontal Position Left: %d m \tHorizontal Position Right: 0 m", magnitude);
+		snprintf(second_row,65,"\r\nHorizontal Position Left: %d m \tHorizontal Position Right: 0 m", magnitude);
 	} else if (horizontal > 0) {
-		snprintf(second_row,65,"\nHorizontal Position Left: 0 m \tHorizontal Position Right: %d m", magnitude);
+		snprintf(second_row,65,"\r\nHorizontal Position Left: 0 m \tHorizontal Position Right: %d m", magnitude);
 	} else {
-		sprintf(second_row, "\nHorizontal Position Left: 0 m \tHorizontal Position Right: 0 m");
+		sprintf(second_row, "\r\nHorizontal Position Left: 0 m \tHorizontal Position Right: 0 m");
 	}
 }
 
 void flightDataThirdRow(char *third_row, char *instruction, uint8_t time) {
-	snprintf(third_row,70,"\nInstruction: %s \tRemaining Time: %d s\n", instruction, time);
+	snprintf(third_row,70,"\r\nInstruction: %s \tRemaining Time: %d s\r\n", instruction, time);
 }
 
 
